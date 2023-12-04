a36ba66dba048c4a5037fc8407370c08da9a25ca
nischal <nischalkshaj5@gmail.com>
2023-12-04 17:25:37 +0530

    new leetcode solved

---
+++ leetcode38.js
@@ -0,0 +1,15 @@
+var countPairs = function (nums, k) {
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] === nums[j] && (i * j) % k === 0) {
+        count++;
+      }
+    }
+  }
+  return count;
+};
+const k = 2;
+const nums = [3, 1, 2, 2, 2, 1, 3];
+const result = countPairs(nums, k);
+console.log(result);
