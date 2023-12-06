d3932927363c9a88c2176d5e28b825305b378999
nischal <nischalkshaj5@gmail.com>
2023-12-06 23:24:24 +0530

    new leet solved on 6th dec

---
+++ 1961. Check If String Is a Prefix of Array.js
@@ -0,0 +1,14 @@
+var isPrefixString = function (s, words) {
+  let compare = "";
+  for (let i = 0; i < words.length; i++) {
+    compare = compare + words[i];
+    if (compare === s) {
+      return true;
+    }
+  }
+  return false;
+};
+const words = ["i", "love", "leetcode", "apples"];
+const s = "iloveleetcode";
+const result = isPrefixString(s, words);
+console.log(result);
