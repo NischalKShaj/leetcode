1b62a9cbdee43dee29c1f5168810c1203dd32edb
nischal <nischalkshaj5@gmail.com>
2024-01-20 09:54:05 +0530

    new leet solved on 2744

--- 125.js
+++ 125.js
@@ -1,12 +1,12 @@
-var isPalindrome = function(s) {
-    const str = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
-    for(let i=0;i<str.length;i++){
-        if(str[i]!==str[str.length-i-1]){
-            return false;
-        }
+var isPalindrome = function (s) {
+  const str = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== str[str.length - i - 1]) {
+      return false;
     }
-    return true;
+  }
+  return true;
 };
 let s = "A man, a plan, a canal: Panama";
-let result = isPalindrome(s)
+let result = isPalindrome(s);
 console.log(result);
---
+++ 2744.js
@@ -0,0 +1,14 @@
+var maximumNumberOfStringPairs = function (words) {
+  let count = 0;
+  for (let i = 0; i < words.length; i++) {
+    for (let j = i + 1; j < words.length; j++) {
+      let rev = words[j].split("").reverse().join("");
+      if (words[i] === rev) {
+        count++;
+      }
+    }
+  }
+  console.log(count);
+};
+let words = ["cd", "ac", "dc", "ca", "zz"];
+let result = maximumNumberOfStringPairs(words);
