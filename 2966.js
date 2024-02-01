391b9af3d39ae3ff2b8a2c141235f68f7018fc33
nischal <nischalkshaj5@gmail.com>
2024-02-02 00:15:11 +0530

    new leet solved on 2nd feb 2024

--- 2966.js
+++ 2966.js
@@ -1,15 +1,15 @@
 var divideArray = function (nums, k) {
-    let arr =[]
-    nums.sort((a,b)=>a-b);
-    for(let i=0;i<nums.length;i+=3){
-        if(nums[i+2]-nums[i]>k){
-            return [];
-        } else{
-            arr.push([nums[i], nums[i + 1], nums[i + 2]]);
-        }
+  let arr = [];
+  nums.sort((a, b) => a - b);
+  for (let i = 0; i < nums.length; i += 3) {
+    if (nums[i + 2] - nums[i] > k) {
+      return [];
+    } else {
+      arr.push([nums[i], nums[i + 1], nums[i + 2]]);
     }
-    console.log(arr);
+  }
+  console.log(arr);
 };
 const nums = [1, 3, 4, 8, 7, 9, 3, 5, 1];
 const k = 3;
-const result = divideArray(nums, k)
+const result = divideArray(nums, k);
